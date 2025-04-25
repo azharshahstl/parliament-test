1fa9d2392c29ca725d12dfcd1b8a9aaadedac0fa
Azhar Shah <azharshahsaintlouis@gmail.com>
2025-04-25 08:16:53 +0100

    create MemberCard class and MemberData interface

---
+++ src/scripts/member-card.ts
@@ -0,0 +1,73 @@
+export interface MemberData {
+  party: string;
+  name: string;
+  constituency: string;
+  isServing: boolean;
+  partyColour: string;
+  thumbnailUrl: string;
+}
+
+export default class MemberCard {
+  private party: string;
+  private name: string;
+  private constituency: string;
+  private isServing: boolean;
+  private partyColour: string;
+  private thumbnailUrl: string;
+
+  constructor({
+    party,
+    name,
+    constituency,
+    isServing,
+    partyColour,
+    thumbnailUrl,
+  }: MemberData) {
+    this.party = party;
+    this.name = name;
+    this.constituency = constituency;
+    this.isServing = isServing;
+    this.partyColour = partyColour;
+    this.thumbnailUrl = thumbnailUrl;
+  }
+
+  render(): HTMLElement {
+    const cardEl = document.createElement("div");
+    cardEl.className = "card";
+
+    const imgEl = document.createElement("img");
+    imgEl.className = "img";
+    imgEl.src = this.thumbnailUrl;
+    imgEl.alt = "";
+    imgEl.style.borderColor = "#" + this.partyColour;
+    cardEl.appendChild(imgEl);
+
+    const contentEl = document.createElement("div");
+    contentEl.className = "content";
+    cardEl.appendChild(contentEl);
+
+    const partyEl = document.createElement("div");
+    partyEl.className = "party";
+    partyEl.textContent = this.party;
+    contentEl.appendChild(partyEl);
+
+    const nameEl = document.createElement("div");
+    nameEl.className = "name";
+    nameEl.textContent = this.name;
+    contentEl.appendChild(nameEl);
+
+    const constituencyEl = document.createElement("div");
+    constituencyEl.className = "constituency";
+    constituencyEl.textContent = this.constituency;
+    contentEl.appendChild(constituencyEl);
+
+    if (!this.isServing) {
+      const noLongerServingEl = document.createElement("div");
+      noLongerServingEl.className = "no-longer-serving";
+      noLongerServingEl.textContent = "No longer serving";
+      contentEl.appendChild(noLongerServingEl);
+    }
+
+    return cardEl;
+  }
+}
